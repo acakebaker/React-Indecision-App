f2ba2353a7d021d407ce077a9840ad1753567c1c
Alexander Baker <38450939+acakebaker@users.noreply.github.com>
2021-12-28 00:17:48 -0600

    Added Delete option function
    12/28/2021

--- indecision/src/App.js
+++ indecision/src/App.js
@@ -5,18 +5,32 @@ import './App.css';
 const App = () => {
   // States
   const [options, setOptions] = useState([]);
+  const [error, setError] = useState(null);
 
   const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer.',
   }
 
+
+  const componentDidMount = () => {
+    console.log('component counted');
+  }
+
+
+
+
   // Submit form function
   const onFormSubmit = (e) => {
     e.preventDefault();
     const option = e.target.elements.option.value.trim();
 
-    if (option) {
+    if(!option) {
+      setError('Please enter an option.');
+    } else if(options.indexOf(option) > -1) {
+      setError('That is already an option.');
+    } else if(option) {
+      setError(null);
       setOptions([...options, option]);
       e.target.elements.option.value = null;
     }
@@ -31,22 +45,25 @@ const App = () => {
     alert('You should do: ' + options[randomNumber]);
   }
 
+  // Deletes a single option from the options state
+  const deleteOption = ({ option }) => setOptions(options.filter(optionFilter => optionFilter !== option));
+
   // The app on the screen
   return (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{options.length > 0 ? 'Here are your options' : 'No options'}</p>
       <form onSubmit={onFormSubmit}>
         <input type='text' name='option' />{' '}
         <button>Add Option</button>
       </form><br />
       <button disabled={options.length <= 1} onClick={onMakeDecision}>What should I do?</button>{' '}
       <button disabled={options.length === 0} onClick={removeAll}>Remove All ({options.length})</button>
+      {error && <p style={{color:'red'}}>{error}</p>}
       <ul>
         {
           options.map((option, key) => {
-            return <li key={key}>{option}</li>
+            return <li key={key}>{option} <button onClick={(e) => deleteOption({option})}>Delete</button></li>
           })
         }
       </ul>
